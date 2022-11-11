a6899b24d1eebcb4a04dde307a4fed58c329af68
guillaume wernert <guil.wernert@gmail.com>
2022-11-11 01:25:01 +0100

    fin quete 3 express

---
+++ users.js
@@ -0,0 +1,105 @@
+const database = require("./database");
+
+const users = [
+{
+    id: 1,
+    firstname:'John',
+    lastname:'Doe',
+    email:'john.doe@example.com',
+    city:'Paris',
+    language:'English'
+},
+{
+    id: 2,
+    firstname:'Valeriy',
+    lastname:'Appius',
+    email:'valeriy.appius@example.com',
+    city:'Moscow',
+    language:'Russian'
+},
+{
+    id: 3,
+    firstname:'Ralf',
+    lastname:'Geronimo',
+    email:'ralf.geronimo@example.com',
+    city:'New York',
+    language:'Italian'
+},
+{
+    id: 4,
+    firstname:'Maria',
+    lastname:'Iskandar',
+    email:'maria.iskandar@example.com',
+    city:'New York',
+    language:'German'
+},
+{
+    id: 5,
+    firstname:'Jzne',
+    lastname:'Doe',
+    email:'jane.doe@example.com',
+    city:'London',
+    language:'English'
+},
+{
+    id: 6,
+    firstname:'Johanna',
+    lastname:'Martino',
+    email:'johanna.martino@example.com',
+    city:'Milan',
+    language:'Spanish'
+},
+    ];
+
+const getUsers = (req, res) => {
+  database
+    .query("select * from users")
+    .then(([users]) => {
+      res.json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+
+const getUserById = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("select * from users where id = ?", [id])
+    .then(([users])=> {
+      if (users[0] !=null){
+        res.json(users[0]);
+      }else{
+        res.status(404).send("Not Found");
+      }
+    })
+    .catch((err)=>{
+      console.error(err);
+      res.status(404).send ("Not found")
+    });
+};
+
+const postUser = (req, res) => {
+    const { firstname, lastname, email, city, language } = req.body;
+
+    database
+      .query(
+        "INSERT INTO users(firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
+        [firstname, lastname, email, city, language]
+      )
+      .then(([result]) => {
+        res.location(`/api/users/${result.insertId}`).sendStatus(201);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error saving the user");
+      });
+  };
+
+module.exports = {
+  getUsers,
+  getUserById,
+  postUser,
+};
