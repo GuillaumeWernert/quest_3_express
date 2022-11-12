d3bc99bad0e3d26492a57d9313a0e3d8125619f2
guillaume wernert <guil.wernert@gmail.com>
2022-11-12 01:34:28 +0100

    quest_5_delete

--- app.js
+++ app.js
@@ -36,3 +36,6 @@ app.post("/api/users", users.postUser);
 
 app.put("/api/movies/:id", movieHandlers.updateMovie);
 app.put("/api/users/:id", users.updateUser);
+
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+app.delete("/api/users/:id", users.deleteUser);
